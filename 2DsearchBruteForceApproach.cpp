4018801384ad20b54cf0f41fbab26da949a04267
Alex Christian <38103724+alex1198@users.noreply.github.com>
2022-01-27 19:52:06 -0500

    Add files via upload

---
+++ 2DsearchBruteForceApproach.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int tar;
+    cin >> tar;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    bool flag = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (arr[i][j] == tar)
+            {
+                flag = true;
+            }
+        }
+    }
+
+    if (flag == true)
+    {
+        cout << "Found";
+    }
+    else
+    {
+        cout << "Not Found";
+    }
+
+    return 0;
+}
